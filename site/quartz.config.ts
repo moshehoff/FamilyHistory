342a5b36b106673e1fb56f74d78d52796e9c8abc
Moshe Hoffman <moshe.hoffman@gmail.com>
2025-11-10 22:58:00 +0200

    fix: Correct baseUrl for GitHub Pages sub-path

--- site/quartz.config.ts
+++ site/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "moshehoff.github.io/FamilyHistory",
+    baseUrl: "/FamilyHistory",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
