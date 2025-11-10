f3341086ba0665310ad0d5aafee014408ad139a3
Moshe Hoffman <moshe.hoffman@gmail.com>
2025-11-10 23:06:50 +0200

    fix: Restore correct baseUrl format for GitHub Pages

--- site/quartz.config.ts
+++ site/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "/FamilyHistory",
+    baseUrl: "moshehoff.github.io/FamilyHistory",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
